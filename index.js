4bb385a4a1be5c65c927b8654afff30e9ca0cc96
Ojbilli <onem08989@gmail.com>
2023-06-21 23:10:29 +0100

    new

--- index.js
+++ index.js
@@ -4,18 +4,43 @@ const PORT = 3000;
 
 const server = http.createServer();
 
-server.on('request', (req, res) => {
-    if(req.url === '/friends'){
-    // res.writeHead( 200, {
-    //     'Content-Type': 'application/json',
-    // });
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
+const friends = [
+    {
+        id:0,
+        name: 'Nikola Tesla',
+    },
+    {
         id:1,
         name: 'Sir Isaac Newton',
-    }));
-}else if (req.url ==='/messages') {
+    },
+    {
+        id:2,
+        name: 'Albert Einsten',
+    },
+]
+
+server.on('request', (req, res) => {
+    const items = req.url.split('/');
+    // /friends/2 => ['', 'friends', '2']
+    if(req.method ==='POST' && items[1] === 'friends'){
+        req.on('data', (data) => {
+            const friend = data.toString();
+            console.log('Request:', friend);
+            friend.push(JSON.parse(friend));
+        });
+        req.pipe(res);
+    }else if(req.method ==='GET' && items[1] === 'friends'){
+   
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    if (items.length ===3) {
+        const friendindex = Number(items[2]);
+        res.end(JSON.stringify(friends [friendindex]));
+    }else {
+        res.end(JSON.stringify(friends));
+    }
+    
+}else if (req.method ==='GET' && items[1] ==='messages') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<body>');
@@ -35,3 +60,9 @@ server.on('request', (req, res) => {
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 });
+
+
+// running on the javascript console for chrome
+// fetch('http://localhost:3000/friends',{ method: 'POST', body: JSON.stringify({id:3, name:'harmza'})})
+// .then((response) => response.json())
+// .then((friend) => console.log(friend));
